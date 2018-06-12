0cfdb670f5d8c7fc56d660c6975292ab3508f2ae
Nick Thomas <nickthomas3245@gmail.com>
2018-06-11 22:29:29 -0500

    In which our hero attempts to remove nouns from the articles

---
+++ scripts/party_started.py
@@ -0,0 +1,15 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+text_w_no_nouns = pickle.load(open("/Users/NickThomas/Project5/pickle_files/text_w_no_nouns.pkl", "rb" ))
+
+short_text = []
+clear = []
+for text in text_w_no_nouns:
+    for part in text:
+        clear.append(part[0])
+    short_text.append(' '.join(clear))
+
+with open('short_text.pkl', 'wb') as picklefile:
+        pickle.dump(short_text, picklefile)
